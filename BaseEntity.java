7dd9a2411f6d7543eb432ef797ad718936eeb4d7
Bungaedelweiss <edelweisssalsabilaa@gmail.com>
2026-01-09 16:37:23 +0700

    Add files via upload

---
+++ BaseEntity.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.Objects;
+
+
+/**
+ *
+ * @author edelweiss
+ */
+public class BaseEntity {
+    protected String id;
+
+    public BaseEntity(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        if (getClass() != o.getClass()) return false;
+        BaseEntity that = (BaseEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
